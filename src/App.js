3c5fa2ad8281261ef46ec87bd4dd0d944fc75def
Sebastián Salas <sebashian961@gmail.com>
2024-12-03 23:29:41 -0600

    init landin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header loading-background">
-        <img style={{marginTop: "0vh"}} src={logo2} className="App-logo" alt="logo" />
+        <img style={{marginTop: "0vh"}} src={logo2} className="App-logo2" alt="logo" />
         <img src={logo} style={{marginTop: "0vw"}} className="App-logo zoom-image" alt="logo" />
       </header>
     </div>
