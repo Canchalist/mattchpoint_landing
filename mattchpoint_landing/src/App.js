92badcb9df58b4a4c7b6af4c219faea4dcf97f41
Sebastián Salas <sebashian961@gmail.com>
2024-12-03 22:21:39 -0600

    init landin

---
+++ mattchpoint_landing/src/App.js
@@ -0,0 +1,14 @@
+import logo2 from './mattchpoint2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header loading-background">
+        <img src={logo2} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
